c474daeeb3d27a01db7a653402983ffa86dfbd93
Ajjima <ajjimajom@gmail.com>
2026-07-06 15:55:57 +0700

    docs: complete mongodb query exercise 1

--- query.mongodb.js
+++ query.mongodb.js
@@ -3,11 +3,13 @@ use("sample_mflix");
 
 // Excercise 1
 // fethch all documents from the comments collection.
-// db.comments.find();
+db.comments.find();
 
 // Fetch only one user from comments collection by _id = ObjectId(“5a9427648b0beebeb69579f5”).
 db.comments.findOne({ _id: ObjectId("5a9427648b0beebeb69579f5") });
 
 // fetch only one user from comments collection by email = “john_bishop@fakegmail.com”.
+db.comments.findOne({ email: "john_bishop@fakegmail.com" });
 
 // fetch only one user from comments collection by name = “John Bishop”.
+db.comments.findOne({ name: "John Bishop" });
