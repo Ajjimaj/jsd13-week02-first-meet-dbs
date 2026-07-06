9a7bc60db8ae1865411e21819c4c370b81fd0f0d
Ajjima <ajjimajom@gmail.com>
2026-07-06 16:12:11 +0700

    docs: complete mongodb query exercise 2

--- query2.mongodb.js
+++ query2.mongodb.js
@@ -1,4 +1,11 @@
 // Connect to the sample_mflix database
 use("sample_mflix");
 
+// Exercise 2
+
+// 2.1 What is an example of  type “movie” and rated “TV-G” look like?
 db.movies.find({ type: "movie", rated: "TV-G" });
+
+// 2.2 How many movies are there in “movie” type and “TV-G” rated?
+db.movies.find({ type: "movie", rated: "TV-G" }).count(); // 58 movies
+// db.movies.countDocuments({ type: "movie", rated: "TV-G" });
